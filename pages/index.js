0d324a87feee532ed661ab7998825433f381d2ab
Zarela Graves <zarelavp@yahoo.com>
2021-10-20 00:54:50 -0400

    Adds card component and example

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import Card from '../components/Card/Card'
 
 export default function Home() {
   return (
@@ -22,33 +23,20 @@ export default function Home() {
         </p>
 
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
 
+        <Card
+            groupName="SWFL Coders"
+            groupDescription="SWFL Coders is a group for techies, developers, coders and non-coders, who wish to learn more about the fantastic world of programming."
+            meetingDate="Meets the 2nd Thursday of each month"
+            groupLink="https://www.meetup.com/SWFL-Coders"
+          />
+
+          {/* replace with another card */}
           <a href="https://nextjs.org/learn" className={styles.card}>
             <h2>Learn &rarr;</h2>
             <p>Learn about Next.js in an interactive course with quizzes!</p>
           </a>
 
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
         </div>
       </main>
 
