56982b08263a2006d8e6c0efc55f5ee54cdf92be
Zarela Graves <zarelavp@yahoo.com>
2021-10-31 23:37:12 -0400

    Adds layout header and footer

---
+++ components/Layout/Layout.jsx
@@ -0,0 +1,12 @@
+import Header from '../Header/Header'
+import Footer from '../Footer/Footer'
+
+const Layout = ({ children }) => (
+  <>
+    <Header />
+      {children}
+    <Footer />
+  </>
+)
+
+export default Layout
