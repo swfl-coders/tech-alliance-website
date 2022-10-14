f44d3967d1ae571fa3fc028f6d1d47c18df53b7e
Madelene Campos <madelenecampos@gmail.com>
2022-10-14 16:49:46 -0500

    Adds meetup and facebook logos that link out from footer

--- components/Footer/Footer.jsx
+++ components/Footer/Footer.jsx
@@ -7,6 +7,25 @@ const Footer = () => {
       <div className={styles.copy}>
         Copyright © <span>{new Date().getFullYear()}</span> Tech Alliance of SWFL. All rights reserved.
       </div>
+
+      <div className={styles.social}>
+        <div className={styles.logos}>
+          <a href="https://www.meetup.com/TechAllianceSWFL/" target="_blank" rel="noreferrer">
+            <img
+              src="/meetup.svg"
+              alt="Meetup logo"
+            />
+          </a>
+        </div>
+        <div className={styles.logos}>
+          <a href="https://www.facebook.com/groups/techswfl" target="_blank" rel="noreferrer">
+            <img
+              src="/facebook.svg"
+              alt="Facebook logo"
+            />
+          </a>
+        </div>
+      </div>
     </footer>
   )
 }
