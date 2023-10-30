93c09959bf71da920d24c9ddb713bcc9bc959882
Zaid Marrie <84665360+ZaidMarrie@users.noreply.github.com>
2023-10-30 11:00:14 +0200

    Updates formatting of indents and semi-colons

--- components/Header/Header.jsx
+++ components/Header/Header.jsx
@@ -1,14 +1,14 @@
-import React from "react";
-import Image from "next/image"
-import logoImage from "../../public/images/logo.png"
-import styles from "./Header.module.css"
+import React from 'react'
+import Image from 'next/image'
+import logoImage from '../../public/images/logo.png'
+import styles from './Header.module.css'
 
 const Header = () => {
-	return (
-		<header className={styles.header}>
-			<Image src={logoImage} alt="" placeholder="blur" priority />
-		</header>
-	)
-};
+  return (
+    <header className={styles.header}>
+      <Image src={logoImage} alt="" placeholder="blur" priority />
+    </header>
+  )
+}
 
 export default Header
