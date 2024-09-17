89dc14748f355ad18911a7d0161fde7d6d5b9084
Jonathan Sanchez <30424887+jonathans199@users.noreply.github.com>
2024-09-17 16:40:10 -0400

    added prettier tailwind and navbar

---
+++ components/home_components/HeroSection.jsx
@@ -0,0 +1,58 @@
+'use client'
+
+import { useState } from 'react'
+import { Dialog, DialogPanel } from '@headlessui/react'
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
+
+const navigation = [
+  { name: 'Product', href: '#' },
+  { name: 'Features', href: '#' },
+  { name: 'Marketplace', href: '#' },
+  { name: 'Company', href: '#' },
+]
+
+export default function HeroSection() {
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
+
+  return (
+    <div className="bg-white">
+      <div className="relative isolate bg-gradient-to-b from-indigo-100/20 pt-14">
+        <div
+          aria-hidden="true"
+          className="absolute inset-y-0 right-1/2 -z-10 -mr-96 w-[200%] origin-top-right skew-x-[-30deg] bg-white shadow-xl shadow-indigo-600/10 ring-1 ring-indigo-50 sm:-mr-80 lg:-mr-96"
+        />
+        <div className="mx-auto max-w-7xl px-6 py-32 sm:py-40 lg:px-8">
+          <div className="mx-auto mt-12 max-w-2xl lg:mx-0 lg:grid lg:max-w-none lg:grid-cols-2 lg:gap-x-16 lg:gap-y-6 xl:grid-cols-1 xl:grid-rows-1 xl:gap-x-8">
+            <h1 className="max-w-2xl text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl lg:col-span-2 xl:col-auto">
+              We’re changing the way people connect.
+            </h1>
+
+            <div className="mt-6 max-w-xl lg:mt-0 xl:col-end-1 xl:row-start-1">
+              <p className="text-lg leading-8 text-gray-600">
+                We are a collective of technology groups and industry leaders dedicated to fostering innovation,
+                promoting technology education, and driving the growth of technology in the Southwest Florida region.
+              </p>
+              <div className="mt-10 flex items-center gap-x-6">
+                <a
+                  href="#"
+                  className="rounded-md bg-orange-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-orange-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+                  Get started
+                </a>
+                <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
+                  Learn more <span aria-hidden="true">→</span>
+                </a>
+              </div>
+            </div>
+
+            <img
+              alt=""
+              src="https://images.unsplash.com/photo-1567532900872-f4e906cbf06a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1280&q=80"
+              className="mt-10 aspect-[6/5] w-full max-w-lg rounded-2xl object-cover sm:mt-16 lg:mt-0 lg:max-w-none xl:row-span-2 xl:row-end-2 xl:mt-36"
+            />
+          </div>
+        </div>
+        <div className="absolute inset-x-0 bottom-0  h-24 bg-gradient-to-t from-white sm:h-32" />
+      </div>
+    </div>
+  )
+}
--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Card from '../components/Card/Card'
 import { groups } from '../utils/config'
+import HeroSection from '../components/home_components/HeroSection'
 
 export default function Home() {
   return (
@@ -12,47 +13,41 @@ export default function Home() {
         <meta name="Tech Alliance" content="Your commuity group" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
+      <HeroSection />
       <main className={styles.main}>
         <div className={styles.about}>
+          <div className="">
+            <img src="/images/logo.png" alt="" className="mx-auto mb-12" />
+          </div>
           <p>
-            We are a collective of technology groups and industry leaders dedicated to fostering innovation, promoting technology education, and driving the growth of technology in the Southwest Florida region.
+            We are a collective of technology groups and industry leaders dedicated to fostering innovation, promoting
+            technology education, and driving the growth of technology in the Southwest Florida region.
           </p>
-          <p>
-            We are non-profit, non-partisan.
-          </p>
+          <p>We are non-profit, non-partisan.</p>
           <p>
             Our alliance comprises the following groups:<br></br>
-            <b style={{color: '#358aca'}}>
-              ✨ SWFL Coders
-              ✨ SWFL Data
-              ✨ SWFL Sec
-              ✨ Python SWFL
-              ✨ VR & AR of Southwest Florida
-              ✨
+            <b style={{ color: '#358aca' }}>
+              ✨ SWFL Coders ✨ SWFL Data ✨ SWFL Sec ✨ Python SWFL ✨ VR & AR of Southwest Florida ✨
             </b>
           </p>
           <p>
-            Our community events are held quarterly, free of charge, and are open to all. We gather at Collaboratory in Downtown Fort Myers, providing a welcoming space for individuals and organizations to come together and share in our passion for technology.
+            Our community events are held quarterly, free of charge, and are open to all. We gather at Collaboratory in
+            Downtown Fort Myers, providing a welcoming space for individuals and organizations to come together and
+            share in our passion for technology.
           </p>
           <div className={styles.logos}>
-            <a target="_blank"  href="https://www.meetup.com/TechAllianceSWFL/" rel="noopener noreferrer">
-            <p>Join us!</p>
-              <Image
-                src="/meetup.svg"
-                alt="Meetup logo"
-                width={40}
-                height={40}
-              />
+            <a target="_blank" href="https://www.meetup.com/TechAllianceSWFL/" rel="noopener noreferrer">
+              <p>Join us!</p>
+              <Image src="/meetup.svg" alt="Meetup logo" width={40} height={40} />
             </a>
           </div>
         </div>
         <div className={styles.grid}>
-        <>
-          {groups.map(group => (
-            <Card key={group.id} {...group} />
-          ))}
-        </>
+          <>
+            {groups.map(group => (
+              <Card key={group.id} {...group} />
+            ))}
+          </>
         </div>
       </main>
     </div>
