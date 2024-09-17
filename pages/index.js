28a518d28c08dde9251a497c93c58a1d749905d6
Jonathan Sanchez <30424887+jonathans199@users.noreply.github.com>
2024-09-17 17:01:14 -0400

    implemented prettier standardization

--- pages/index.js
+++ pages/index.js
@@ -16,43 +16,34 @@ export default function Home() {
       <main className={styles.main}>
         <div className={styles.about}>
           <p>
-            We are a collective of technology groups and industry leaders dedicated to fostering innovation, promoting technology education, and driving the growth of technology in the Southwest Florida region.
+            We are a collective of technology groups and industry leaders dedicated to fostering innovation, promoting
+            technology education, and driving the growth of technology in the Southwest Florida region.
           </p>
-          <p>
-            We are non-profit, non-partisan.
-          </p>
+          <p>We are non-profit, non-partisan.</p>
           <p>
             Our alliance comprises the following groups:<br></br>
-            <b style={{color: '#358aca'}}>
-              ✨ SWFL Coders
-              ✨ SWFL Data
-              ✨ SWFL Sec
-              ✨ Python SWFL
-              ✨ VR & AR of Southwest Florida
-              ✨
+            <b style={{ color: '#358aca' }}>
+              ✨ SWFL Coders ✨ SWFL Data ✨ SWFL Sec ✨ Python SWFL ✨ VR & AR of Southwest Florida ✨
             </b>
           </p>
           <p>
-            Our community events are held quarterly, free of charge, and are open to all. We gather at Collaboratory in Downtown Fort Myers, providing a welcoming space for individuals and organizations to come together and share in our passion for technology.
+            Our community events are held quarterly, free of charge, and are open to all. We gather at Collaboratory in
+            Downtown Fort Myers, providing a welcoming space for individuals and organizations to come together and
+            share in our passion for technology.
           </p>
           <div className={styles.logos}>
-            <a target="_blank"  href="https://www.meetup.com/TechAllianceSWFL/" rel="noopener noreferrer">
-            <p>Join us!</p>
-              <Image
-                src="/meetup.svg"
-                alt="Meetup logo"
-                width={40}
-                height={40}
-              />
+            <a target="_blank" href="https://www.meetup.com/TechAllianceSWFL/" rel="noopener noreferrer">
+              <p>Join us!</p>
+              <Image src="/meetup.svg" alt="Meetup logo" width={40} height={40} />
             </a>
           </div>
         </div>
         <div className={styles.grid}>
-        <>
-          {groups.map(group => (
-            <Card key={group.id} {...group} />
-          ))}
-        </>
+          <>
+            {groups.map(group => (
+              <Card key={group.id} {...group} />
+            ))}
+          </>
         </div>
       </main>
     </div>
