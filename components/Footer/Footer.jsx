c50d180f69a5d47e0b155a35090bd4bee57082a9
Dayna-Marie Pemberton <daynamariejpemberton@gmail.com>
2024-10-22 10:05:42 -0400

    Add missing socials (#80)

--- components/Footer/Footer.jsx
+++ components/Footer/Footer.jsx
@@ -5,30 +5,71 @@ import Image from 'next/image'
 const Footer = () => {
   return (
     <footer className={styles.footer}>
-      <div className={styles.copy}>
-        Copyright © <span>{new Date().getFullYear()}</span> Tech Alliance of SWFL. All rights reserved.
-      </div>
+
       <div className={styles.social}>
         <div className={styles.logos}>
           <a href="https://www.meetup.com/TechAllianceSWFL/" target="_blank" rel="noreferrer">
             <Image
-              src="/meetup.svg"
+              src="/images/socials/mu-icon.png"
               alt="Meetup logo"
-              width={30}
-              height={30}
+              width={40}
+              height={40}
             />
           </a>
         </div>
         <div className={styles.logos}>
           <a href="https://www.facebook.com/techallianceswfl" target="_blank" rel="noreferrer">
             <Image
-              src="/facebook.svg"
+              src="/images/socials/fb-icon.png"
               alt="Facebook logo"
-              width={30}
-              height={30}
+              width={40}
+              height={40}
             />
           </a>
         </div>
+        <div className={styles.logos}>
+          <a href="https://twitter.com/tech_swfl" target="_blank" rel="noreferrer">
+            <Image 
+              src="/images/socials/x-social.png"
+              alt="X/Twitter Logo"
+              width={40}
+              height={40}
+            />
+          </a>
+        </div>
+        <div className={styles.logos}>
+          <a href="https://discord.gg/G5UR26qAbT" target="_blank" rel="noreferrer">
+            <Image 
+              src="/images/socials/dis-icon.png"
+              alt="Discord Logo"
+              width={40}
+              height={40}
+            />
+          </a>
+        </div>
+        <div className={styles.logos}>
+          <a>
+            <Image 
+              src="/images/socials/ig-icon.png"
+              alt="Instagram Logo"
+              width={40}
+              height={40}
+            />
+          </a>
+        </div>
+        <div className={styles.logos}>
+          <a href="https://www.youtube.com/@techallianceswfl" target="_blank" rel="noreferrer">
+            <Image 
+              src="/images/socials/yt-icon.png"
+              alt="Youtube Logo"
+              width={40}
+              height={40}
+            />
+          </a>
+        </div>
+      </div>
+      <div className={styles.copy}>
+        Copyright © <span>{new Date().getFullYear()}</span> Tech Alliance of SWFL. All rights reserved.
       </div>
     </footer>
   )
