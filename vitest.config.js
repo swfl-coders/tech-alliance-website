7768b0e15584af79279840a392a98e7bfbfc3e15
davidstevenrose <33883868+davidstevenrose@users.noreply.github.com>
2024-10-28 01:01:00 -0400

    Added support for js-dom testing. Added faker-js to dev deps (#86)
    Co-authored-by: CORP\drose <>

--- vitest.config.js
+++ vitest.config.js
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
- 
+
 export default defineConfig({
   plugins: [react()],
   test: {
+    globals: true,
     environment: 'jsdom',
-  },
+    setupFiles: ['./__tests__/setup/setup']
+  }
 })
