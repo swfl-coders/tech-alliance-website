0ee5bd4e228bbec7c4c88a8dfbadfb0c9dfc5c05
Logan <logan.w.nguyen2026@gmail.com>
2026-06-08 16:04:40 -0400

    Removed references to Python Group

--- utils/config.js
+++ utils/config.js
@@ -10,16 +10,6 @@ export const groups = [
   },
   {
     id: 2,
-    groupName: 'Python SWFL',
-    groupDescription:
-      'Python SWFL is a volunteer-run interdisciplinary user group for exploration and experimentation with the Python programming language. Whether you are a seasoned software engineer or a complete beginner, all levels are welcome! ',
-    meetingDate: 'Meets the last Tuesday of each month',
-    groupLink: 'https://www.meetup.com/pythonswfl',
-    xLink: 'https://twitter.com/PythonSWFL',
-    linkedinLink: 'https://www.linkedin.com/company/pythonswfl/',
-  },
-  {
-    id: 3,
     groupName: 'SWFL Coders',
     groupDescription:
       'SWFL Coders is a group for techies, developers, coders and non-coders, who wish to learn more about the fantastic world of programming. Our meetings are held twice a month, one at our official home the Collaboratory in Downtown Fort Myers and the other one in Naples or Bonita Springs.',
@@ -31,7 +21,7 @@ export const groups = [
     githubLink: 'https://github.com/swfl-coders',
   },
   {
-    id: 4,
+    id: 3,
     groupName: 'SWFL Hackerspace',
     groupDescription:
       'Hackerspaces are places where tech geeks, experts and nerds meet up to talk, build, and hack technology. Join the Southwest Florida Hackerspace if you feel you can contribute to the group or want to learn more about technology.',
@@ -42,7 +32,7 @@ export const groups = [
     linkedinLink: 'https://www.linkedin.com/company/swfl-hackerspace/',
   },
   {
-    id: 5,
+    id: 4,
     groupName: 'SWFL Sec - Southwest Florida Infosec Meetup',
     groupDescription:
       "The mission of SWFLSec is to provide a free, vendor-neutral forum for the expansion and dissemination of industry knowledge, to extend the culture of security awareness, and enable members to make informed, and educated security decisions. SWFLSec runs on attendee participation. If you've built, broken, or discovered something cool, please consider sharing with the group.",
@@ -54,7 +44,7 @@ export const groups = [
     instagramLink: 'https://www.instagram.com/swflsec/',
   },
   {
-    id: 7,
+    id: 5,
     groupName: 'VR & AR of Southwest Florida',
     groupDescription:
       'Our group welcomes VR/AR creators and enthusiasts of Southwest Florida and beyond! We welcome everyone, including innovators, developers, inventors, storytellers, content producers, entrepreneurs, hackers, programmers, pioneers, and adventurers. Join us to be in touch with ARVR news, events and learn something new to get your development to the next level.',
